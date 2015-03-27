eae231a84a972c373ad60110aec627cdcc7a900a
Igor Dimitrijevic <igor@backbase.com>
2015-03-28 00:11:38 +0100

    eslint applied

--- src/mosaic-rest-js.js
+++ src/mosaic-rest-js.js
@@ -1,89 +1,90 @@
+/*global getRequestBody, stringToJs*/
 (function(window){
-
+'use strict';
 
     function extend() {
 	var i, j, a = arguments;
 	for (i = 1; i < a.length; i++) {
-	    for (j in a[i]) a[0][j] = a[i][j];
+            for (j in a[i]) a[0][j] = a[i][j];
 	}
 	return a[0];
     }
 
     function BBRest(cnf) {
 	this.config = extend({
-	    host: 'localhost',
-	    port: '7777',
-	    context: 'portalserver',
-	    username: 'admin',
-	    password: 'admin', // TODO: do not expose password for frontend version
+            host: 'localhost',
+            port: '7777',
+            context: 'portalserver',
+            username: 'admin',
+            password: 'admin', // TODO: do not expose password for frontend version
             plugin: null,
-	    portal: null
+            portal: null
 	}, cnf || {});
     }
 
     extend(BBRest.prototype, {
 	server: function() {
-	    return new BBReq('server', this.config, ['portals']);
+            return new BBReq('server', this.config, ['portals']);
 	},
 	portal: function() {
-	    var a = ['portals', this.config.portal];
-	    return new BBReq('portal', this.config, a);
+            var a = ['portals', this.config.portal];
+            return new BBReq('portal', this.config, a);
 	},
         catalog: function(item) {
             var a = ['catalog'];
             if (item) a.push(item);
-	    return new BBReq('server', this.config, a);
+            return new BBReq('server', this.config, a);
         },
         portalCatalog: function(item) {
             var a = ['portals', this.config.portal, 'catalog'];
             if (item) a.push(item);
-	    return new BBReq('portal', this.config, a);
+            return new BBReq('portal', this.config, a);
         },
 	container: function(containerName) {
-	    var a = ['portals', this.config.portal, 'containers'];
-	    if (containerName) a.push(containerName);
-	    return new BBReq('container', this.config, a);
+            var a = ['portals', this.config.portal, 'containers'];
+            if (containerName) a.push(containerName);
+            return new BBReq('container', this.config, a);
 	},
 	widget: function(widgetName) {
-	    var a = ['portals', this.config.portal, 'widgets'];
-	    if (widgetName) a.push(widgetName);
-	    return new BBReq('widget', this.config, a);
+            var a = ['portals', this.config.portal, 'widgets'];
+            if (widgetName) a.push(widgetName);
+            return new BBReq('widget', this.config, a);
 	},
 	page: function(pageName) {
-	    var a = ['portals', this.config.portal, 'pages'];
-	    if (pageName) a.push(pageName);
-	    return new BBReq('page', this.config, a);
+            var a = ['portals', this.config.portal, 'pages'];
+            if (pageName) a.push(pageName);
+            return new BBReq('page', this.config, a);
 	},
 	link: function(linkName) {
-	    var a = ['portals', this.config.portal, 'links'];
-	    if (linkName) a.push(linkName);
-	    return new BBReq('link', this.config, a);
+            var a = ['portals', this.config.portal, 'links'];
+            if (linkName) a.push(linkName);
+            return new BBReq('link', this.config, a);
 	},
 	user: function(userName, showGroups, groupName) {
-	    var a = ['users'];
-	    if (userName) a.push(userName);
-	    if (showGroups) a.push('groups');
-	    if (groupName) a.push(groupName);
-	    return new BBReq('user', this.config, a);
+            var a = ['users'];
+            if (userName) a.push(userName);
+            if (showGroups) a.push('groups');
+            if (groupName) a.push(groupName);
+            return new BBReq('user', this.config, a);
 	},
 	group: function(groupName, showUsers, userName) {
-	    var a = ['groups'];
-	    if (groupName) a.push(groupName);
-	    if (showUsers) a.push('users');
-	    if (userName) a.push(userName);
-	    return new BBReq('group', this.config, a);
+            var a = ['groups'];
+            if (groupName) a.push(groupName);
+            if (showUsers) a.push('users');
+            if (userName) a.push(userName);
+            return new BBReq('group', this.config, a);
 	},
 	template: function(templateName) {
-	    var a = ['templates'];
-	    if (templateName) a.push(templateName);
-	    return new BBReq('template', this.config, a);
+            var a = ['templates'];
+            if (templateName) a.push(templateName);
+            return new BBReq('template', this.config, a);
 	},
 	audit: function(meta) {
-	    return new BBReq('audit', this.config, [meta? 'auditmeta' : 'auditevents']);
+            return new BBReq('audit', this.config, [meta ? 'auditmeta' : 'auditevents']);
 	},
 	cache: function(type) {
-	    var a = ['caches', type];
-	    return new BBReq('cache', this.config, a);
+            var a = ['caches', type];
+            return new BBReq('cache', this.config, a);
 	},
 	import: function() {
 	},
@@ -91,11 +92,11 @@
 	},
         auto: function(d, method) {
             var t = this;
-	    return getRequestBody(d, this.config.plugin).then(function(r) {
-                if (typeof r === "string") r = stringToJs(r);
+            return getRequestBody(d, this.config.plugin).then(function(r) {
+                if (typeof r === 'string') r = stringToJs(r);
                 var a = t.jxonToObj(r, method);
                 return t[a[0]]()[a[1]](d);
-            });        
+            });
         },
         jxonToObj: function(j, method) {
             var aKey, bKey, plural, context, a = [],
@@ -143,24 +144,24 @@
 	this.uri = uri;
 	this.qs = {};
 	this.headers = {
-	    "Content-Type": "application/xml"
+            'Content-Type': 'application/xml'
 	};
     }
 
     extend(BBReq.prototype, {
 	rights: function() {
-	    this.uri.push('rights');
-	    return this;
+            this.uri.push('rights');
+            return this;
 	},
 	tag: function(tagName, tagType) {
-	    this.uri.push('tags');
-	    if (tagName) this.uri.push(tagName);
+            this.uri.push('tags');
+            if (tagName) this.uri.push(tagName);
             if (tagType) this.qs.type = tagType;
-	    return this;
+            return this;
 	},
 	query: function(o) {
-	    this.qs = o;
-	    return this;
+            this.qs = o;
+            return this;
 	},
 	get: function() {
             /* methods that use .xml:
@@ -170,21 +171,21 @@
              * widget('name').xml().get()
              * page('name').xml().get()
              * link('name').xml().get() */
-	    if (this.uri[0] === 'portals' && this.uri.length === 2) this.uri[1] += '.xml';
-	    if (this.uri[2] === 'catalog' && this.uri[3]) this.uri[3] += '.xml';
-	    if (this.uri[2] === 'pages' && this.uri[3]) this.uri[3] += '.xml';
-	    if (this.uri[2] === 'containers' && this.uri[3]) this.uri[3] += '.xml';
-	    if (this.uri[2] === 'widgets' && this.uri[3]) this.uri[3] += '.xml';
-	    if (this.uri[2] === 'links' && this.uri[3]) this.uri[3] += '.xml';
-	    this.method = 'GET';
-	    return this.req();
+            if (this.uri[0] === 'portals' && this.uri.length === 2) this.uri[1] += '.xml';
+            if (this.uri[2] === 'catalog' && this.uri[3]) this.uri[3] += '.xml';
+            if (this.uri[2] === 'pages' && this.uri[3]) this.uri[3] += '.xml';
+            if (this.uri[2] === 'containers' && this.uri[3]) this.uri[3] += '.xml';
+            if (this.uri[2] === 'widgets' && this.uri[3]) this.uri[3] += '.xml';
+            if (this.uri[2] === 'links' && this.uri[3]) this.uri[3] += '.xml';
+            this.method = 'GET';
+            return this.req();
 	},
 	post: function(d) {
-	    this.method = 'POST';
+            this.method = 'POST';
             return this.doRequest(d);
 	},
 	put: function(d) {
-	    this.method = 'PUT';
+            this.method = 'PUT';
             return this.doRequest(d);
 	},
 	// fixing inconsistencies in API
@@ -192,25 +193,25 @@
 	// portal /portals/[portal_name]/delete/catalog POST
 	// link /portals/[portal_name]/delete/links POST
 	delete: function(v) {
-	    this.method = 'DELETE';
-	    if (v) {
+            this.method = 'DELETE';
+            if (v) {
 		this.method = 'POST';
 		switch (this.command) {
-		    case 'server':
+                    case 'server':
 			this.uri = ['delete', 'catalog'];
 			break;
-		    case 'portal':
+                    case 'portal':
 			this.uri[2] = 'delete';
 			this.uri[3] = 'catalog';
 			break;
-		    case 'link':
+                    case 'link':
 			this.uri[2] = 'delete';
 			this.uri[3] = 'links';
 			break;
-		    default:
-		       // code
+                    default:
+                       // code
 		}
-	    }
+            }
             if (this.command === 'cache' && this.uri[1] === 'all') {
                 return this.deleteAllCache(0);
             }
@@ -218,7 +219,7 @@
 	},
         doRequest: function(d) {
             var t = this;
-	    return getRequestBody(d, this.config.plugin).then(function(r) {
+            return getRequestBody(d, this.config.plugin).then(function(r) {
                 return t.req(r);
             });
         },
@@ -250,6 +251,6 @@ var cch = ['globalModelCache',
         'versionBundleCache'];
 
 /* include */
-	
-	
+
+
 })(this);
